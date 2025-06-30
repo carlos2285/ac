4e2a1a143e00e51f3b9931a055ae72176f93c3a0
carlos2285 <59510768+carlos2285@users.noreply.github.com>
2025-06-30 16:48:21 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
-export default {
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       colors: {
         'primary-navy': '#263366',
         'secondary-gray': '#5A5A5F',
-        'light-blue': '#B1C2E2',
-        'lightest-blue': '#EFF8FB',
+        'light-blue':   '#B1C2E2',
+        'lightest-blue':'#EFF8FB',
         'accent-green': '#D9E9CE',
       },
       fontFamily: {
-        grotesk: ['"Space Grotesk"', 'sans-serif']
+        grotesk: ['"Space Grotesk"', 'sans-serif'],
       },
       keyframes: {
         float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-20px)' },
+          '0%,100%': { transform: 'translateY(0)' },
+          '50%':     { transform: 'translateY(-20px)' },
         },
       },
       animation: {
